66b3764c69e5101cd4eb5c7555aabcc1dd464ed0
KevinBaoB <kevinbaobui@gmail.com>
2022-06-07 04:57:20 -0500

    Translated

--- python/pig_latin.py
+++ python/pig_latin.py
@@ -1,3 +1,35 @@
-def translate(word_or_phrase):
-  pass
 
+import re
+def translate(word_or_phrase):
+  vowels = ['a','e','i','o','u']
+  words = word_or_phrase.split()
+
+  for i, word in enumerate(words):
+
+    if word[0] in vowels:
+        words[i] = words[i]+ "ay"
+    else:
+        has_vowel = False
+        
+        for j, letter in enumerate(word):
+            if letter in vowels:
+                  if letter == 'u':
+                    words[i] = word[j + 1:] + word[:j + 1]
+                    has_vowel == True
+                  else :
+                    words[i] = word[j:] + word[:j] + "ay"
+                    has_vowel = True
+                    break
+        if(has_vowel == False):
+            words[i] = words[i]+ "ay"
+
+  pig_latin = ' '.join(words)
+  return pig_latin
+    
+
+    
+    
+
+
+# print(translate("apple university"))
+# print(translate("square"))
